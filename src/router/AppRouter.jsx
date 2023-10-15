824556d959b16fad79aa3bab81862c49568a906e
AlbertoLcm <alberto.cm4007@gmail.com>
2023-10-15 09:08:56 -0600

    suspense deleted

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,21 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import { routes } from "../helpers/routes";
-import { Suspense, lazy } from "react";
 import Finishes from "../pages/Finishes";
 
-const Home = lazy(() => import("../pages/Home"));
-const Cleaning = lazy(() => import("../pages/Cleaning"));
-const Facilities = lazy(() => import("../pages/Facilities"));
+import Home from "../pages/Home";
+import Cleaning from "../pages/Cleaning";
+import Facilities from "../pages/Facilities";
 
 export default function AppRouter() {
   return (
-    <Suspense fallback={<p>Cargando...</p>}>
-      <Routes>
-        <Route path={routes.home} element={<Home />} />
-        <Route path={routes.cleaning} element={<Cleaning />} />
-        <Route path={routes.finishes} element={<Finishes />} />
-        <Route path={routes.facilities} element={<Facilities />} />
-      </Routes>
-    </Suspense>
+    <Routes>
+      <Route path={routes.home} element={<Home />} />
+      <Route path={routes.cleaning} element={<Cleaning />} />
+      <Route path={routes.finishes} element={<Finishes />} />
+      <Route path={routes.facilities} element={<Facilities />} />
+    </Routes>
   );
 }
